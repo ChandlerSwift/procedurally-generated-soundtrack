ba7f96d1fdaa9c12b49fc62a53347fb905dec7b6
Chandler Swift <chandler@chandlerswift.com>
2022-01-12 18:03:37 -0600

    Tweak values for better sound

--- play.py
+++ play.py
@@ -6,23 +6,26 @@ fs = fluidsynth.Synth()
 fs.start()
 
 def bassline():
+    VELOCITY=40
     while True:
-        yield [(36, 100)], 2
-        yield [(38, 100)], 2
+        yield [(36, VELOCITY)], 2
+        yield [(38, VELOCITY)], 2
 
 def tenor():
+    VELOCITY=40
     while True:
         yield [], 0.5
-        yield [(52, 100), (55, 100), (59, 100)], 0.25
+        yield [(52, VELOCITY), (55, VELOCITY), (59, VELOCITY)], 0.25
         yield [], 0.75
-        yield [(52, 100), (55, 100), (59, 100)], 0.25
+        yield [(52, VELOCITY), (55, VELOCITY), (59, VELOCITY)], 0.25
         yield [], 0.75
-        yield [(53, 100), (57, 100), (60, 100)], 0.25
+        yield [(53, VELOCITY), (57, VELOCITY), (60, VELOCITY)], 0.25
         yield [], 0.75
-        yield [(53, 100), (57, 100), (60, 100)], 0.25
+        yield [(53, VELOCITY), (57, VELOCITY), (60, VELOCITY)], 0.25
         yield [], 0.25
 
 def melody():
+    VELOCITY=80
     random.seed(0) # consistent randomness for debugging
     # a premade bit of melody
     # while True:
@@ -31,13 +34,14 @@ def melody():
     #     yield [(55, 100)], 4
     notes = [60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84]
     last_note_index = 4 # gotta start somewhere
+    yield [(notes[last_note_index], 100)], 0.5
     while True:
         offset = random.choice([-2, -1, -1, 0, 1, 1, 2])
         if 0 <= last_note_index + offset < len(notes):
             last_note_index = last_note_index + offset
-            yield [(notes[last_note_index], 100)], 0.5
+            yield [(notes[last_note_index], VELOCITY)], 0.25
 
-BPM=72
+BPM=60
 
 def play(notes_stream):
     while True:
@@ -50,7 +54,7 @@ def play(notes_stream):
 
 # available from https://sites.google.com/site/soundfonts4u/
 sfid = fs.sfload("soundfonts/Essential Keys-sforzando-v9.6.sf2")
-fs.program_select(0, sfid, 0, 0)
+fs.program_select(0, sfid, 0, 8)
 
 import threading # hacks, TODO: as generator?
 bass_thread = threading.Thread(target=play, args=(bassline(),))
