2da2d5de480b9729372364a96df4376638d8ee7f
Chandler Swift <chandler@chandlerswift.com>
2022-01-13 17:48:35 -0600

    Add style module framework

--- play.py
+++ play.py
@@ -1,7 +1,6 @@
 import time
 from typing import List
 import fluidsynth
-import random
 import functools
 import heapq
 
@@ -9,42 +8,6 @@ import heapq
 
 fs = fluidsynth.Synth()
 fs.start()
-
-def bassline():
-    VELOCITY=40
-    while True:
-        yield [(36, VELOCITY)], 2
-        yield [(38, VELOCITY)], 2
-
-def tenor():
-    VELOCITY=40
-    while True:
-        yield [], 0.5
-        yield [(52, VELOCITY), (55, VELOCITY), (59, VELOCITY)], 0.25
-        yield [], 0.75
-        yield [(52, VELOCITY), (55, VELOCITY), (59, VELOCITY)], 0.25
-        yield [], 0.75
-        yield [(53, VELOCITY), (57, VELOCITY), (60, VELOCITY)], 0.25
-        yield [], 0.75
-        yield [(53, VELOCITY), (57, VELOCITY), (60, VELOCITY)], 0.25
-        yield [], 0.25
-
-def melody(seed: int=0):
-    VELOCITY=80
-    random.seed(seed) # consistent randomness for debugging
-    # a premade bit of melody
-    # while True:
-    #     for note in "71 72 71 72 71 67 64 67 69 65 62 69 67 64 57 59".split():
-    #         yield [(int(note), 100)], 0.25
-    #     yield [(55, 100)], 4
-    notes = [60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84]
-    last_note_index = 4 # gotta start somewhere
-    yield [(notes[last_note_index], 100)], 0.5
-    while True:
-        offset = random.choice([-2, -1, -1, 0, 1, 1, 2])
-        if 0 <= last_note_index + offset < len(notes):
-            last_note_index = last_note_index + offset
-            yield [(notes[last_note_index], VELOCITY)], 0.25
 
 @functools.total_ordering
 class Event:
@@ -62,12 +25,11 @@ class Event:
     def __str__(self) -> str:
         return f"{self.event_type}@{self.start_time:.3f}: {self.key_id}@{self.velocity}"
 
-BPM=60
 DEBUG=False
 
 # TODO: I think fluidsynth.Sequencer would make a lot of this easier?
 # TODO: synchronize with real time; could be free with fluidsynth.Sequencer
-def play(*parts):
+def play(*parts, BPM: int):
     q: List(Event) = [] # empty heap
     current_time = 0
     searched_until = 0
@@ -106,8 +68,10 @@ def play(*parts):
 sfid = fs.sfload("soundfonts/Essential Keys-sforzando-v9.6.sf2")
 fs.program_select(0, sfid, 0, 8)
 
+import styles.peaceful as style
+
 try:
-    play(bassline(), tenor(), melody())
+    play(*style.parts, BPM=style.BPM)
 except KeyboardInterrupt:
     pass
 
